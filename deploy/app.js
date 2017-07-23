d5274e84ea221ab4200f6889ea791705dc2c3f83
Vital Krasilnikau <vital.krasilnikau@gmail.com>
2017-07-23 13:30:37 +0300

    Added gzip support

--- deploy/app.js
+++ deploy/app.js
@@ -1,10 +1,11 @@
 var express = require('express'),
 	app = express(),
+	expressStaticGzip = require("express-static-gzip"),
 	isProd = typeof(process) !== 'undefined' && process && process.env && process.env.PORT,
 	port = isProd ? process.env.PORT : 3000, // Heroku should pass process.env.PORT
 	rootFolder = __dirname;
 
-app.use(express.static(rootFolder));
+app.use(expressStaticGzip(rootFolder));
 
 app.get('/', function(req, res){
 	res.sendFile(`${rootFolder}/index.html`);
